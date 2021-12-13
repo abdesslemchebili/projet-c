75ea72d661c0819aba1b48e69b4b132de6dd3fcc
chedyakhichi <chedya.khichi@esprit.tn>
2021-12-13 11:33:19 +0100

    src chedya

---
+++ crud.h
@@ -0,0 +1,9 @@
+
+void ajout( capteur c);
+int exist(char*ref);
+void supp(char*ref);
+
+
+
+/************************/
+
---
+++ tree.h
@@ -0,0 +1,6 @@
+void Capteurtree(GtkWidget* treeview1,char*l);
+int ChercherCapteurTree(GtkWidget* treeview1,char*l,char*type);
+void Debittree(GtkWidget* treeview1,char*l);
+void Temptree(GtkWidget* treeview1,char*l);
+void Fumeetree(GtkWidget* treeview1,char*l);
+
